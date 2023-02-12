1d946bf089f07051b123598cf3e3101377bbf329
emines0 <emin.sefa1@gmail.com>
2023-02-12 11:47:22 +0100

    useRef for the toggle  navigation

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,22 +1,44 @@
 /* eslint-disable jsx-a11y/anchor-has-content */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState, useRef, useEffect } from 'react'
-import { FaBars, FaTwitter } from 'react-icons/fa'
+import { FaBars } from 'react-icons/fa'
 import { links, social } from '../../data'
 import logo from '../../logo.svg'
 
 const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false)
+  const linksContainerRef = useRef(null) // for div
+  const linksRef = useRef(null) // for ul
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`
+    } else {
+      linksContainerRef.current.style.height = '0px'
+    }
+  }, [showLinks])
+
+  /*
+   * every time if showLinks change check the height of the links
+   * if showLinks true set the height of the links container to linksHeight
+   * else showLinks false set the height of the links to 0px
+   */
+
   return (
     <nav>
       <div className='nav-center'>
         <div className='nav-header'>
           <img src={logo} alt='logo' />
-          <button className='nav-toggle'>
+          <button
+            className='nav-toggle'
+            onClick={() => setShowLinks(!showLinks)}
+          >
             <FaBars />
           </button>
         </div>
-        <div className='links-container show-container'>
-          <ul className='links'>
+        <div className='links-container' ref={linksContainerRef}>
+          <ul className='links' ref={linksRef}>
             {links.map((link) => {
               const { id, url, text } = link
               return (
@@ -27,6 +49,7 @@ const Navbar = () => {
             })}
           </ul>
         </div>
+
         <ul className='social-icons'>
           {social.map((link) => {
             const { id, url, icon } = link
